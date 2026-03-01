06d86e67d0c7d3a57ea7cc3d8aa37128c47c74d6
Joro716 <joro716@vidalibarraquer.net>
2026-03-01 13:54:13 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Act_1._4___Wiki___Controls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
+    }
+}
